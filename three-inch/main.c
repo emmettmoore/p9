f69a60d3d293ab7f2f2b217929cc3eae78730522
cmalchik <cmalchik@gmail.com>
2015-02-17 02:26:08 -0500

    trying to get a simple write/read working... no success yet.

--- three-inch/main.c
+++ three-inch/main.c
@@ -1,8 +1,19 @@
 #define THREEINCH
 #include "stuff.h"
+#include <stdio.h>
 
 void
 main(void) {
+	char *buf = "hello";
+	char c;
+	Queue *q;
+	q = qopen(1024, 0, 0, 0);
+	qwrite(q, buf, 5);
+	print("hey\n");
+	qread(q, &c, 1);
+	print("did\n");
+	printf("%s\n", buf);
+	printf("buf2: %c\n", c);
 	print("woooo! compiles and links !!!!\n");
 	exits(0);
 }
