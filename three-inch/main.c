31ae51287e34444972955f62a4cb6d5d237e955b
cmalchik <cmalchik@gmail.com>
2015-02-27 18:11:40 -0500

    rfork working. huge delay (>100s) before consumer does anything - stopped in qread?

--- three-inch/main.c
+++ three-inch/main.c
@@ -2,21 +2,31 @@
 #include "stuff.h"
 #include <stdio.h>
 
-void
-main(void) {
-    print("in main, haven't done anything\n");
-	char *buf = "hello";
+void main(void)
+{
+	int pid, val, i;
 	char c;
-	Queue *q;
-    print("declared some shit\n");
-	q = qopen(1024, 0, 0, 0);
-    print("qopen success\n");
-	qwrite(q, buf, 5);
-	print("qwrite success\n");
-	qread(q, &c, 1);
-	print("qread success\n");
-	printf("%s\n", buf);
-	printf("buf2: %c\n", c);
-	print("woooo! three inchez!!!!\n");
-	exits(0);
+	char *buf = "hello";
+	char *buf2 = "a";
+	Queue *q = qopen(1024, 0, 0, 0);
+	qwrite(q, buf2, 1);
+
+	switch(pid = rfork(RFPROC|RFMEM)){
+	case -1: /* fork failed */
+		print("ABORT!\n");
+		abort();
+	case 0: /* child: consumer */
+		printf("C: here!\n");
+		for(i = 0; i < 5; i++){
+			val = qread(q, &c, 1);
+			printf("C: qread returned %d\n", val);
+			printf("C: read %c\n", c);
+		}
+		break;
+	default: /* parent: producer */
+		val = qwrite(q, buf, 5);
+		printf("P: qwrite returned %d\n", val);
+		printf("P: wrote %s\n", buf);
+		waitpid();
+	}
 }
